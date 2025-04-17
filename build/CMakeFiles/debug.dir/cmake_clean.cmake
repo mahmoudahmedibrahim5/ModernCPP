af657869192632d7a6ebeb3ba74147b331ecc810
Mahmoud Ahmed <mahmoudahmedibrahim5@gmail.com>
2025-04-17 15:47:02 +0200

    Setup the environment, cmake, and debug configuration

---
+++ build/CMakeFiles/debug.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/debug.dir/link.d"
+  "CMakeFiles/debug.dir/src/main.cpp.o"
+  "CMakeFiles/debug.dir/src/main.cpp.o.d"
+  "debug"
+  "debug.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/debug.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
